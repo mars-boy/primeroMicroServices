23f65cc764daf8b04eda1f5796f4c6e9a46f9c6c
mars-boy <akhilvantipalli@gmail.com>
2018-02-05 07:12:42 +0530

    Cleaned Code

--- AccountService/src/main/java/com/marsboy/microservices/primero/AccountService/Controller/AccountServiceViewController.java
+++ AccountService/src/main/java/com/marsboy/microservices/primero/AccountService/Controller/AccountServiceViewController.java
@@ -6,7 +6,6 @@ import com.netflix.discovery.shared.Application;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpServletResponse;
@@ -23,11 +22,6 @@ public class AccountServiceViewController {
         Application application = eurekaClient.getApplication("expenses-service");
         InstanceInfo instanceInfo = application.getInstances().get(0);
         String url = "http://"+instanceInfo.getIPAddr()+ ":"+instanceInfo.getPort()+"/"+"ExpenseService/expenses";
-        //return new ModelAndView("redirect:"+url);
-        /*httpServletResponse.setHeader("Location", "http://192.168.0.104:6001/ExpenseService/expenses");*/
-        System.out.println("Groot------------------------------------------------------------------------------------");
-        System.out.println("url :::: "+url);
-        System.out.println("------------------------------------------------------------------------------------------");
         RedirectView redirectView = new RedirectView();
         redirectView.setUrl(url);
         return redirectView;
